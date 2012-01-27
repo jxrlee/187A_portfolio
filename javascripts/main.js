bc2a45ff428cb107bcce299dd69923619d57a6c0
Joseph Lee <jxrlee@gmail.com>
2012-01-26 22:04:40 -0800

    new logos

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,38 @@
+//Execute the function when window load
+$(window).bind("load", function() { 
+
+	   //setup the height and position for your sticky footer
+       var footerHeight = 0,
+           footerTop = 0,
+           $footer = $("#footer");
+
+       positionFooter();
+
+       function positionFooter() {
+
+                footerHeight = $footer.height();
+                footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+
+               if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+                   $footer.css({
+                        position: "absolute"
+                   }).animate({
+                        top: footerTop
+                   })
+               } else {
+                   $footer.css({
+                        position: "static"
+                   })
+               }
+
+       }
+
+       $(window)
+               .scroll(positionFooter)
+               .resize(positionFooter)
+
+});
+
 $(document).ready(function() {
 
   /*Put your Javascript code here*/
